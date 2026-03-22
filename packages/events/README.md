23eedd6c42e4a01738e9a5c209a75589b124369f
Yannick Bochatay <yannick.bochatay@free.fr>
2026-03-22 07:50:57 +0100

    add jsyg/events

---
+++ packages/events/README.md
@@ -0,0 +1,8 @@
+# @jsyg/events
+Events constructor for [JSYG framework](https://github.com/YannickBochatay/jsyg-monorepo) (or standalone).
+
+
+## Installation
+```shell
+npm install @jsyg/events
+```
---
+++ packages/events/tests/tests.js
@@ -0,0 +1,37 @@
+import Events from "../index.js"
+
+const { module, test } = QUnit
+
+module("Events");
+
+test("Manipulation des événements", assert => {     
+    
+    let cpt = 0;
+    
+    const events = new Events();
+    
+    function incremente() { cpt++; }
+    
+    events.ontest = null;
+    
+    assert.expect(4);
+    
+    events.on("test",incremente);
+    events.trigger("test");
+    assert.equal(cpt,1,"Déclenchement de l'événement");
+    
+    events.on("test",incremente);
+    events.trigger("test");
+    assert.equal(cpt,2,"Non prise en compte des doublons");
+    
+    events.off("test",incremente);
+    events.trigger("test");
+    assert.equal(cpt,2,"Suppression d'un événement");
+    
+    events.one("test",incremente);
+    events.trigger("test");
+    events.trigger("test");
+    events.trigger("test");
+    assert.equal(cpt,3,"Méthode one qui retire aussitot l'événement");
+    
+});
