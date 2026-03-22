5c4dac1051b19fec6b15dba1e7b93873c4cb8fc9
Yannick Bochatay <yannick.bochatay@free.fr>
2026-03-22 08:00:52 +0100

    add stdconstruct

---
+++ packages/stdconstruct/tests/tests.js
@@ -0,0 +1,18 @@
+import StdConstruct from "../index.js";
+
+const { module, test } = QUnit
+
+module("StdConstruct");
+
+test("Gestion des fonction standard", assert => {     
+    
+    var obj = new StdConstruct();
+    
+    obj.enable();
+    
+    assert.expect(2);
+    
+    assert.equal(obj.enabled, true, "activation du plugin");
+    
+    assert.equal(typeof obj.on, "function", "héritage de Events");
+});
