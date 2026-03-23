9f0c4a4b8c4a748e0be23ab87f0eb10bac9fc7b7
BOCHATAY YANNICK <yannick.bochatay@meteo.fr>
2026-03-23 09:46:31 +0100

    dynamic import map

---
+++ docs/importmap.js
@@ -0,0 +1,36 @@
+(()=>{
+  // inspired by https://lea.verou.me/blog/2026/external-import-maps-today/
+
+  const DIR = "../node_modules";
+
+  const map = { imports : {
+    jquery : DIR + "/jquery/dist-module/jquery.module.min.js",
+    pathseg : DIR + "/pathseg/pathseg.js"
+  } };
+
+  const jsygModules = ["core","events","matrix","path","pathdrawer","point","resizable","stdconstruct","strutils","utils","vect","wrapper"];
+	
+  for (const module of jsygModules) {
+    map.imports[`@jsyg/${module}`] = `${DIR}/@jsyg/${module}/index.js`;
+  }
+
+  const mapUrl = document.currentScript?.src;
+
+  if (mapUrl) {
+    // transform relative urls to absolute
+    for (const name in map.imports) {
+      map.imports[name] = new URL(map.imports[name], mapUrl);
+    }
+
+    const script = Object.assign(document.createElement("script"), {
+      type: "importmap",
+      textContent: JSON.stringify(map)
+    });
+
+    document.currentScript?.after(script);
+
+  } else {
+    console.error("Import map injector script must be a classic (non-module) script");
+  }
+
+})();
