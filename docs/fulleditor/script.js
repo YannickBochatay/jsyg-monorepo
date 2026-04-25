46f842081c5cefd9072ed227372cca9f64ad7bfc
BOCHATAY YANNICK <yannick.bochatay@meteo.fr>
2026-04-25 22:44:54 +0200

    continue jsygfulleditor

--- docs/fulleditor/script.js
+++ docs/fulleditor/script.js
@@ -7,24 +7,117 @@ svgEditor.editableShapes = "> *";
 
 svgEditor.enable();
 
-svgEditor.newDocument(500,500);
+svgEditor.newDocument(500, 500);
 
 svgEditor.enableDropFiles();
 
 svgEditor.enableMouseWheelZoom();
 
-$("#newDocument").on("click",function() {
-    svgEditor.newDocument( $('#width').val(), $('#height').val() );
+svgEditor.on("load", function () {
+  const dim = svgEditor.dimDocument();
+  $('#width').val(dim.width);
+  $('#height').val(dim.height);
 });
 
-$("#openDocument").on("click",function() {
-    svgEditor.chooseFile().then(svgEditor.loadFile).catch(alert);
+const menuBar = document.querySelector("menu-bar");
+
+svgEditor.registerKeyShortCut({
+  "ctrl+c": svgEditor.copy,
+  "ctrl+x": svgEditor.cut,
+  "ctrl+v": svgEditor.paste,
+  "ctrl+z": svgEditor.undo,
+  "ctrl+y": svgEditor.redo,
+  "ctrl+a":svgEditor.selectAll,
+  "del": svgEditor.remove,
+  "up" : function(e) { e.preventDefault(); svgEditor.dim("y","-=1"); },
+  "down" : function(e) { e.preventDefault(); svgEditor.dim("y","+=1"); },
+  "left" : function(e) { e.preventDefault(); svgEditor.dim("x","-=1"); },
+  "right" : function(e) { e.preventDefault(); svgEditor.dim("x","+=1"); }
 });
 
-$("#openImage").on("click",function() {
-    svgEditor.chooseFile().then(svgEditor.loadImageAsDoc).catch(alert);
+////////////////////////////////
+// Menu File
+
+$("#newDocument").on("click", function () {
+  svgEditor.newDocument($('#width').val(), $('#height').val());
 });
 
+$("#openDocument").on("click", function () {
+  svgEditor.chooseFile().then(svgEditor.loadFile).catch(alert);
+});
+
+$("#openImage").on("click", function () {
+  svgEditor.chooseFile().then(svgEditor.loadImageAsDoc).catch(alert);
+});
+
+$("#downloadSVG").on("click",function() {
+  svgEditor.download("svg");
+});
+
+$("#downloadPNG").on("click",function() {
+  svgEditor.download("png");
+});
+
+////////////////////////////////
+// Menu View
+
+$('#marqueeZoom').on("click",function() {
+    svgEditor.marqueeZoom();
+    menuBar.close();
+});
+
+$('#fitToCanvas').on("click",function() {
+    svgEditor.zoomTo('canvas');
+});
+
+$('#fitToDoc').on("click",function() {
+    svgEditor.fitToDoc();
+});
+
+$('#realSize').on("click",function() {
+    svgEditor.zoomTo(100);
+});
+
+$('#zoomIn').on("click",function() {
+    svgEditor.zoom(+10);
+});
+
+$('#zoomOut').on("click",function() {
+    svgEditor.zoom(-10);
+});
+
+////////////////////////////////
+// Menu Options
+
+["canvasResizable", "editPathMainPoints", "editPathCtrlPoints",
+  "keepShapesRatio", "autoSmoothPaths", "useTransformAttr",
+  "editPosition", "editSize", "editRotation", "editText"].forEach(function (property) {
+
+    $('#' + property).on("change", function () {
+      svgEditor[property] = this.checked;
+      $(this).blur();
+    }).trigger("change");
+  });
+
+////////////////////////////////
+// Menu Disposition
+["left","center","right","top","middle","bottom"].forEach(function(type) {
+  $('#align'+$.ucfirst(type)).on("click",function() {
+    svgEditor.align(type);
+  })
+});
+
+["Forwards","Backwards","Front","Back"].forEach(function(type) {
+  $('#move'+type).on("click",function() {
+    svgEditor["move"+type]();
+  });
+});
+
+$("#insertText").on("click",function() {
+  const text = $("<text>").text("Bonjour le monde");
+  svgEditor.enableInsertElement(text);
+  menuBar.close();
+});
 
 /*
 const dialog = document.querySelector('#exampleChoice');
@@ -34,25 +127,9 @@ $('#openExample').on("click",function() {
 });
 
 
-["left","center","right","top","middle","bottom"].forEach(function(type) {
 
-    $('#align'+$.ucfirst(type)).on("click",function() {
-        svgEditor.align(type);
-    })
-});
 
-["Front","Back","ToFront","ToBack"].forEach(function(type) {
 
-    $('#move'+type).on("click",function() {
-        svgEditor["moveTarget"+type]();
-    });
-});
-
-$("#insertText").on("click",function() {
-    const text = $("<text>").text("Bonjour le monde");
-    svgEditor.enableInsertElement(text);
-    $(this).trigger("blur");
-});
 
 
 $("#insertImage").on("click",function() {
@@ -60,15 +137,7 @@ $("#insertImage").on("click",function() {
     svgEditor.chooseFile().then(svgEditor.insertImageFile).catch(alert);
 });
 
-$("#downloadSVG").on("click",function() {
 
-    svgEditor.download("svg");
-});
-
-    $("#downloadPNG").on("click",function() {
-
-    svgEditor.download("png");
-});
 
 
 
@@ -77,11 +146,7 @@ $('#confirmExample').on("click",function() {
     svgEditor.loadURL('img/' + $('#examples').val() + '.svg');
 });
 
-svgEditor.on("load",function() {
-    const dim = svgEditor.dimDocument();
-    $('#width').val(dim.width);
-    $('#height').val(dim.height);
-});
+
 
 $('#width').on("change",function() {
     svgEditor.dimDocument({width:this.value});
@@ -127,29 +192,6 @@ $('#shape').on("change",function() {
     else svgEditor.enableShapeDrawer(shape);
 });
 
-$('#marqueeZoom').on("click",function() {
-    svgEditor.marqueeZoom();
-});
-
-$('#fitToCanvas').on("click",function() {
-    svgEditor.zoomTo('canvas');
-});
-
-$('#fitToDoc').on("click",function() {
-    svgEditor.fitToDoc();
-});
-
-$('#realSize').on("click",function() {
-    svgEditor.zoomTo(100);
-});
-
-$('#zoomIn').on("click",function() {
-    svgEditor.zoom(+10);
-});
-
-$('#zoomOut').on("click",function() {
-    svgEditor.zoom(-10);
-});
 
 ["remove","copy","cut","paste","undo","redo","group","ungroup"].forEach(function(action) {
 
@@ -158,33 +200,10 @@ $('#zoomOut').on("click",function() {
     });
 });
 
-["canvasResizable","editPathMainPoints","editPathCtrlPoints","keepShapesRatio","autoSmoothPaths","useTransformAttr","editPosition","editSize","editRotation","editText"].forEach(function(property) {
 
-    $('#'+property).on("change",function() {
-        svgEditor[property] = this.checked;
-        $(this).blur();
-    }).trigger("change");
-});
 
 $('#print').on("click",function() { svgEditor.print(); });
 
-svgEditor.registerKeyShortCut({
-    "ctrl+c": svgEditor.copy,
-    "ctrl+x": svgEditor.cut,
-    "ctrl+v": svgEditor.paste,
-    "ctrl+z": svgEditor.undo,
-    "ctrl+y": svgEditor.redo,
-    "ctrl+a":svgEditor.selectAll,
-    "del": svgEditor.remove,
-    "up" : function(e) { e.preventDefault(); svgEditor.dim("y","-=1"); },
-    "down" : function(e) { e.preventDefault(); svgEditor.dim("y","+=1"); },
-    "left" : function(e) { e.preventDefault(); svgEditor.dim("x","-=1"); },
-    "right" : function(e) { e.preventDefault(); svgEditor.dim("x","+=1"); }
-});
 
-svgEditor.newDocument(500,500);
 
-svgEditor.enableDropFiles();
-
-svgEditor.enableMouseWheelZoom();
  */
